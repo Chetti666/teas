6d6f9d2801840592b70af35b0eee722dad059ea3
Chetti666 <a.guiachetti@gmail.com>
2026-07-06 16:17:49 -0400

    SEO

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,7 @@ import tailwind from '@astrojs/tailwind'
 import vercel from '@astrojs/vercel'
 
 export default defineConfig({
+  site: 'https://teas.cl',
   integrations: [react(), tailwind()],
   adapter: vercel(),
   output: 'static',
